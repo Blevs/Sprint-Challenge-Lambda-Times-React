648236359c33bc76611cf2d390caa320aa0eae71
Henry Blevins <heblevi@gmail.com>
2019-03-29 12:02:09 -0400

    Implement Carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
-import { carouselData } from '../../data'
+import { carouselData } from '../../data';
 // Complete this Carousel 
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
-    }
+      imgs: [],
+      current: 0,
+    };
   }
   componentDidMount(){
-
+    this.setState({imgs: carouselData});
   }
 
   leftClick = () => {
-
+    this.setState(({imgs, current}) => ({
+      current: (imgs.length + current - 1) % imgs.length
+    }));
   }
 
   rightClick = () => {
-
+    this.setState(({imgs, current}) => ({
+      current: (current + 1) % imgs.length
+    }));
   }
-
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        <img src={this.state.imgs[this.state.current]}
+             style={{display: 'block'}}
+             alt="" />
       </div>
-    )
+    );
   }
 }
